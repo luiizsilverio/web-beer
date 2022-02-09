8883c11b6069c57ce93fc09a4159f70c890c1e4a
luiizsilverio <luiiz.silverio@gmail.com>
2022-02-09 19:41:15 -0300

    erro 504

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,52 +34,52 @@ export default function Home(props: Props) {
   )
 }
 
-export async function getServerSideProps(context) {
-  const cookies = Nookies.get(context)
-  let token = cookies['MyBeer:token']
-  let senha = cookies['MyBeer:senha']
+// export async function getServerSideProps(context) {
+//   const cookies = Nookies.get(context)
+//   let token = cookies['MyBeer:token']
+//   let senha = cookies['MyBeer:senha']
 
-  // se não existir o cookie de senha ou de token, vai para a tela de login
-  if (!senha || !token) {
-    return {
-      redirect: {
-        destination: '/signin',
-        permanent: false,
-      }
-    }
-  }
+//   // se não existir o cookie de senha ou de token, vai para a tela de login
+//   if (!senha || !token) {
+//     return {
+//       redirect: {
+//         destination: '/signin',
+//         permanent: false,
+//       }
+//     }
+//   }
 
-  // verifica se o token é válido ou se não expirou
-  try {
-    verify(token as string, process.env.NEXT_PUBLIC_API_SECRET)
-  }
-  catch(err) {
-    console.log('**', err?.message) // jwt expired
-    token = ''
-  }
+//   // verifica se o token é válido ou se não expirou
+//   try {
+//     verify(token as string, process.env.NEXT_PUBLIC_API_SECRET)
+//   }
+//   catch(err) {
+//     console.log('**', err?.message) // jwt expired
+//     token = ''
+//   }
   
-  // se o token for inválido ou expirou, vai para a tela de login
-  if (!token) {
-    return {
-      redirect: {
-        destination: '/signin',
-        permanent: false,
-      }
-    }
-  }
+//   // se o token for inválido ou expirou, vai para a tela de login
+//   if (!token) {
+//     return {
+//       redirect: {
+//         destination: '/signin',
+//         permanent: false,
+//       }
+//     }
+//   }
 
-  // descriptografa a senha do cookie
-  try {
-    const bytes = CryptoJS.AES.decrypt(senha, process.env.NEXT_PUBLIC_API_SECRET);
-    senha = bytes.toString(CryptoJS.enc.Utf8);    
-  }
-  catch(err) {
-    console.log('**', err) // invalid signature
-    senha = ''
-  }  
+//   // descriptografa a senha do cookie
+//   try {
+//     const bytes = CryptoJS.AES.decrypt(senha, process.env.NEXT_PUBLIC_API_SECRET);
+//     senha = bytes.toString(CryptoJS.enc.Utf8);    
+//   }
+//   catch(err) {
+//     console.log('**', err) // invalid signature
+//     senha = ''
+//   }  
 
-  // envia a senha descriptografada para a Home
-  return {
-    props: { senha }
-  }
-}
+//   // envia a senha descriptografada para a Home
+//   return {
+//     props: { senha }
+//   }
+// }
